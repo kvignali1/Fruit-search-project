ab86972ab832678d02b63e8313af92cdcdcc5f7a
Kevin Vignali <kvchopper@gmail.com>
2023-07-19 14:26:05 -0700

    Adding/Modifying JS code for dropdown

---
+++ script.js
@@ -0,0 +1,127 @@
+const fruit = [
+	"Apple",
+	"Apricot",
+	"Avocado 🥑",
+	"Banana",
+	"Bilberry",
+	"Blackberry",
+	"Blackcurrant",
+	"Blueberry",
+	"Boysenberry",
+	"Currant",
+	"Cherry",
+	"Coconut",
+	"Cranberry",
+	"Cucumber",
+	"Custard apple",
+	"Damson",
+	"Date",
+	"Dragonfruit",
+	"Durian",
+	"Elderberry",
+	"Feijoa",
+	"Fig",
+	"Gooseberry",
+	"Grape",
+	"Raisin",
+	"Grapefruit",
+	"Guava",
+	"Honeyberry",
+	"Huckleberry",
+	"Jabuticaba",
+	"Jackfruit",
+	"Jambul",
+	"Juniper berry",
+	"Kiwifruit",
+	"Kumquat",
+	"Lemon",
+	"Lime",
+	"Loquat",
+	"Longan",
+	"Lychee",
+	"Mango",
+	"Mangosteen",
+	"Marionberry",
+	"Melon",
+	"Cantaloupe",
+	"Honeydew",
+	"Watermelon",
+	"Miracle fruit",
+	"Mulberry",
+	"Nectarine",
+	"Nance",
+	"Olive",
+	"Orange",
+	"Clementine",
+	"Mandarine",
+	"Tangerine",
+	"Papaya",
+	"Passionfruit",
+	"Peach",
+	"Pear",
+	"Persimmon",
+	"Plantain",
+	"Plum",
+	"Pineapple",
+	"Pomegranate",
+	"Pomelo",
+	"Quince",
+	"Raspberry",
+	"Salmonberry",
+	"Rambutan",
+	"Redcurrant",
+	"Salak",
+	"Satsuma",
+	"Soursop",
+	"Star fruit",
+	"Strawberry",
+	"Tamarillo",
+	"Tamarind",
+	"Yuzu",
+];
+
+const textInput = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+textInput.onkeyup = function () {
+	let result = [];
+	let input = textInput.value;
+	if (input.length) {
+		result = fruit.filter((keyword) => {
+			return keyword.toLowerCase().includes(input.toLowerCase());
+		});
+		// console.log(result);
+	}
+	showSuggestions(result);
+	if (!result.length) {
+		textInput.innerHTML = "";
+	}
+};
+
+function showSuggestions(result) {
+	const content = result.map((list) => {
+		return "<li onclick=useSuggestion(this)>" + list + "</li>";
+	});
+	suggestions.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function useSuggestion(list) {
+	console.log(list.innerHTML);
+	textInput.value = list.innerHTML;
+	suggestions.innerHTML = "";
+}
+
+function search(str) {
+	let results = [];
+
+	// TODO
+
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+}
+
+textInput.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
